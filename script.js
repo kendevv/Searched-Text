0de9ff98c36fe163caeab737dde12db93c6366b7
unknown <abdelhakim@univ-tiaret.dz>
2021-06-05 22:38:59 +0100

    get byid

--- script.js
+++ script.js
@@ -2,5 +2,8 @@
 
 function search(){
     let textToSearch = document.getElementById("text-to-search").value;
+    let paragraph = document.getElementById("paragraph");
    
+
+    paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
 }
